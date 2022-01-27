74ac291d0587c7736059d2198e55fed7479ce711
Skrypkin Yevhenii <skrypkin.e@gmail.com>
2022-01-27 20:15:17 +0200

    First files add

---
+++ js/script.js
@@ -0,0 +1,11 @@
+if(window.matchMedia('(min-width: 769px)').matches){
+    $(document).ready(function(){       
+        $('.team-image').slick({
+            dots: true,
+            // infinite: true,
+            speed: 700,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            
+            
+        })})}
