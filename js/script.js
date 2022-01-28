414647d5dae332647ae983ffb75c49510eac1cef
Skrypkin Yevhenii <skrypkin.e@gmail.com>
2022-01-28 09:05:35 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-if(window.matchMedia('(min-width: 769px)').matches){
-    $(document).ready(function(){       
-        $('.team-image').slick({
-            dots: true,
-            // infinite: true,
-            speed: 700,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false
-            
-})})}
-
+$(document).ready(function(){
+    window.addEventListener('resize', function() {  
+            $('.team-image').slick({
+                dots: true,
+                speed: 700,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                responsive: [
+                    {
+                        breakpoint: 769,
+                        settings: 'unslick'
+                    }
+                    
+                ]                
+    })}
+)
+})
 let year = new Date().getFullYear()
 document.querySelector('#year').innerHTML = year
