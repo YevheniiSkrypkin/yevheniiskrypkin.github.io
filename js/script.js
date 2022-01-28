6ee40e4539287976cceca2f61425a61ca0cabd6b
Skrypkin Yevhenii <skrypkin.e@gmail.com>
2022-01-28 09:28:08 +0200

    slick responsive

--- js/script.js
+++ js/script.js
@@ -13,5 +13,20 @@ $(document).ready(function(){
         ]
     })
 })
+window.addEventListener('resize', function(){
+    $('.team-image').slick({
+        dots: true,
+        speed: 700,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,                
+        responsive: [
+            {
+            breakpoint: 769,
+            settings: 'unslick'
+            }
+        ]
+    })
+} )
 let year = new Date().getFullYear()
 document.querySelector('#year').innerHTML = year
